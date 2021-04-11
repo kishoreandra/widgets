4f18e02582b154a945d866cb04756fb5efdd5fac
kishoreandra <keechu1008@gmail.com>
2021-04-11 14:05:41 +0530

    bug fix on dropdown open close state

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,25 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [activeState, setActiveState] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (e) => {
+      if (ref.current.contains(e.target)) {
+        return null;
+      }
+      setActiveState(false);
+    };
+    document.body.addEventListener("click", onBodyClick, {
+      capture: true,
+    });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
   const renderedList = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -17,13 +35,20 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select a Team</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          className={`ui selection dropdown ${
+            activeState ? "visible active" : ""
+          } `}
+          onClick={() => setActiveState(!activeState)}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedList}</div>
+          <div className={`menu ${activeState ? "visible transition" : ""}`}>
+            {renderedList}
+          </div>
         </div>
       </div>
     </div>
